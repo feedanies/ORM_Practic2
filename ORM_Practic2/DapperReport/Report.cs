af6e556001e0e828372ef317425b022b9dbede67
feedanies <fidansdova590@gmail.com>
2026-09-20 05:23:50 +0400

    commit

--- ORM_Practic2/DapperReport/Report.cs
+++ ORM_Practic2/DapperReport/Report.cs
@@ -11,7 +11,7 @@ namespace ORM_Practic2.DapperReport
     {
         private readonly string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PublishDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False;Command Timeout=30";
 
-        public IEnumerable<Author> GetAuthorBooks()
+        public IEnumerable<AuthorDapper> GetAuthorBooks()
         {
             string sql = @"SELECT A.Fullname,A.Age,B.AuthorId,B.Title,B.Genre
                             FROM Authors AS A
@@ -25,7 +25,7 @@ namespace ORM_Practic2.DapperReport
                     a.Books.Add(b);
                     b.Author = a;
                     return a;
-                }, splitOn: nameof(BookDapper.AuthorId);
+                }, splitOn: nameof(BookDapper.AuthorId));
             }
         }
 
